ea7ab164d8c8ff6b6517899de19e1f7acb995a4e
Joy Pratt <joympratt@gmail.com>
2014-10-26 23:43:44 -0500

    complete tutorial part 2

--- dist/js/game.js
+++ dist/js/game.js
@@ -15,7 +15,56 @@ window.onload = function () {
 
   game.state.start('boot');
 };
-},{"./states/boot":2,"./states/gameover":3,"./states/menu":4,"./states/play":5,"./states/preload":6}],2:[function(require,module,exports){
+},{"./states/boot":4,"./states/gameover":5,"./states/menu":6,"./states/play":7,"./states/preload":8}],2:[function(require,module,exports){
+'use strict';
+
+var Bird = function(game, x, y, frame) {
+  Phaser.Sprite.call(this, game, x, y, 'bird', frame);
+
+  this.anchor.setTo(0.5, 0.5);
+
+  this.animations.add('flap');
+  this.animations.play('flap', 12, true);
+
+  this.game.physics.arcade.enableBody(this);
+
+};
+
+Bird.prototype = Object.create(Phaser.Sprite.prototype);
+Bird.prototype.constructor = Bird;
+
+Bird.prototype.update = function() {
+
+  // write your prefab's specific update code here
+
+};
+
+module.exports = Bird;
+
+},{}],3:[function(require,module,exports){
+'use strict';
+
+var Ground = function(game, x, y, width, height) {
+  Phaser.TileSprite.call(this, game, x, y, width, height, 'ground');
+
+  this.autoScroll(-200, 0);
+  this.game.physics.arcade.enableBody(this);
+
+  this.body.allowGravity = false;
+  this.body.immovable = true;
+};
+
+Ground.prototype = Object.create(Phaser.TileSprite.prototype);
+Ground.prototype.constructor = Ground;
+
+Ground.prototype.update = function() {
+
+
+};
+
+module.exports = Ground;
+
+},{}],4:[function(require,module,exports){
 
 'use strict';
 
@@ -34,7 +83,7 @@ Boot.prototype = {
 
 module.exports = Boot;
 
-},{}],3:[function(require,module,exports){
+},{}],5:[function(require,module,exports){
 
 'use strict';
 function GameOver() {}
@@ -62,7 +111,7 @@ GameOver.prototype = {
 };
 module.exports = GameOver;
 
-},{}],4:[function(require,module,exports){
+},{}],6:[function(require,module,exports){
 
 'use strict';
 function Menu() {}
@@ -92,40 +141,45 @@ Menu.prototype = {
     this.startButton = this.game.add.button(this.game.width/2, 300, 'startButton', this.startClick, this);
     this.startButton.anchor.setTo(0.5, 0.5);
   },
+  startClick: function() {
+    this.game.state.start('play');
+  },
   update: function() {
   }
 };
 
 module.exports = Menu;
 
-},{}],5:[function(require,module,exports){
+},{}],7:[function(require,module,exports){
 
   'use strict';
+
+  var Bird = require('../prefabs/bird');
+  var Ground = require('../prefabs/ground');
+
   function Play() {}
   Play.prototype = {
     create: function() {
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
-      this.sprite.inputEnabled = true;
-      
-      this.game.physics.arcade.enable(this.sprite);
-      this.sprite.body.collideWorldBounds = true;
-      this.sprite.body.bounce.setTo(1,1);
-      this.sprite.body.velocity.x = this.game.rnd.integerInRange(-500,500);
-      this.sprite.body.velocity.y = this.game.rnd.integerInRange(-500,500);
+      this.game.physics.arcade.gravity.y = 500;
 
-      this.sprite.events.onInputDown.add(this.clickListener, this);
+      this.background = this.game.add.sprite(0, 0, 'background');
+
+      this.bird = new Bird(this.game, 100, this.game.height/2);
+      this.game.add.existing(this.bird);
+
+      this.ground = new Ground(this.game, 0, 400, 335, 112);
+      this.game.add.existing(this.ground);
+
     },
     update: function() {
-
+      this.game.physics.arcade.collide(this.bird, this.ground);
     },
-    clickListener: function() {
-      this.game.state.start('gameover');
-    }
   };
-  
+
   module.exports = Play;
-},{}],6:[function(require,module,exports){
+
+},{"../prefabs/bird":2,"../prefabs/ground":3}],8:[function(require,module,exports){
 
 'use strict';
 function Preload() {
@@ -152,7 +206,7 @@ Preload.prototype = {
   },
   update: function() {
     if(!!this.ready) {
-      this.game.state.start('menu');
+      this.game.state.start('play');
     }
   },
   onLoadComplete: function() {
